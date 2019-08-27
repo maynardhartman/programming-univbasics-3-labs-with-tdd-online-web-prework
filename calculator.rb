adb42d71c1a62cdb485d7a8a3959b25c3f89b756
Maynard Hartman <mjhartman@brighthouse.com>
2019-08-27 11:56:10 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
-first_number = 0.0
-second_number = 0.0
+first_number = 1.0
+second_number = 1.0
 sum = first_number + second_number
 difference = first_number - second_number 
 product = first_number * second_number
